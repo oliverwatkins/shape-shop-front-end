2dcbaca9b98ef2f82c3a8457f9b8a6b2470b209d
Oliver Watkins <oliver.f.watkins@gmail.com>
2020-06-03 13:57:17 +0200

    login styling

--- src/login/LoginScreen.js
+++ src/login/LoginScreen.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { createLoginAction } from './loginActions';
-import { isUserLoggedIn } from '../selectors';
-import { Link } from 'react-router-dom';
-
+import {connect} from 'react-redux';
+import {createLoginAction} from './loginActions';
+import {isUserLoggedIn} from '../selectors';
+import {Link} from 'react-router-dom';
+import "./login.scss"
 
 
 export class BasicLoginForm extends React.PureComponent {
@@ -11,84 +11,49 @@ export class BasicLoginForm extends React.PureComponent {
 	constructor() {
 		super();
 		this.state = {
-		username: '',
-		password: '',
-	};
+			username: '',
+			password: '',
+		};
 	}
 
 	handleChange = (e) => {
-		const { name, value } = e.currentTarget;
-		this.setState({ [name]: value });
+		const {name, value} = e.currentTarget;
+		this.setState({[name]: value});
 	};
 
 	onSubmit = (e) => {
 		e !== undefined && e.preventDefault();
-		const { username, password } = this.state;
+		const {username, password} = this.state;
 		this.props.onSubmit(username, password);
 	};
-
-
-	// handleChange = (e) => {
-	// 	const { name, value } = e.currentTarget;
-	// 	this.setState({ [name]: value });
-	// };
-	//
-	// onSubmit = (e) => {
-	// 	e !== undefined && e.preventDefault();
-	// 	const { username, password } = this.state;
-	// 	this.props.onSubmit(username, password);
-	// };
 
 	render() {
 		if (this.props.isUserLoggedIn)
 			return <span>Logged in</span>;
 
-		const { username, password } = this.state;
+		const {username, password} = this.state;
 
-		return (
-				<div className="content__elem content__elem--sidebar sidebar">
-					<form className="setup login sidebar__row sidebar__row--content" onSubmit={this.onSubmit}>
-						<div className="sidebar__elem sidebar__elem--content sidebar__elem--narrow">
-							<div>
-								<div>
-									<label className="setup__label">{'user name : '}</label>
-									<input
-										className="setup__input input"
-										name={'username'}
-										type={'text'}
-										value={username}
-										onChange={this.handleChange}
-										placeholder={'placeholder.email'}
-									/>
-								</div>
-								<div>
-									<label className="setup__label">{'password'}</label>
-									<input
-										className="setup__input input"
-										name={'password'}
-										type={'password'}
-										value={password}
-										onChange={this.handleChange}
-										placeholder={'password'}
-									/>
-								</div>
-								<div>
-									<button
-										className="login__button button button--secondary button--sml button--radius"
-										type="submit"
-									>
-										Login
-									</button>
-								</div>
-							</div>
-						</div>
+		return <form onSubmit={this.onSubmit} className={"loginForm"}>
+			<div className="container">
+				<label htmlFor="uname"><b>Username</b></label>
+				<input type="text" placeholder="Enter Username" name="username" value={username} onChange={this.handleChange} required/>
 
-					</form>
-				</div>
-		);
+				<label htmlFor="psw"><b>Password</b></label>
+				<input type="password" placeholder="Enter Password" name="password" value={password} onChange={this.handleChange} required/>
+
+				<button type="submit">Login</button>
+				<label>
+					<input type="checkbox" checked="checked" name="remember"/> Remember me
+				</label>
+			</div>
+
+			<div className="container" style={{backgroundColor: "#f1f1f1"}}>
+				<button type="button" className="cancelbtn">Cancel</button>
+				<span className="psw">Forgot <a href="#">password?</a></span>
+			</div>
+		</form>;
 	}
 }
-
 
 
 const mapStateToProps = state => {
