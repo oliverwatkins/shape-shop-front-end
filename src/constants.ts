6d338e4acdcad96b6538b73c7d8889c012e1ea59
oliverwatkins <oliver.f.watkins@gmail.com>
2023-01-23 06:34:20 +0100

    temp

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,26 @@
 
 
-export const company = "alpenhof";
+export const company = "higgins";
 
-export const baseURL = "http://localhost:8080/";
+
+
+/* two container use : */
+// export const baseURL = "http://localhost:8080/";
+
+/* 'docker compose up' use : */
+export const baseURL = "http://localhost:8081/";
+
+
+
+// export const baseURL = "https://shapeshop-shapeshop.azuremicroservices.io/";
+
+
 
 
 // switches -
 
 // persist in local storage
-export const CACHE = false;
+export const CACHE = true;
 
 // mock mode
 export const MOCK_MODE = false;
-
